60966ffff4c919b7254c991d68f74b484332cc79
volnxebec <huanzhong.wei@mail.utoronto.ca>
2016-03-02 04:02:57 -0500

    cleaned unused variable in files

--- src/amazonwebcrawler/AmazonWebCrawler.java
+++ src/amazonwebcrawler/AmazonWebCrawler.java
@@ -15,9 +15,6 @@ import java.util.Set;
  */
 public class AmazonWebCrawler {
 
-    private static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
-    
     /**
      * @param args the command line arguments
      */
